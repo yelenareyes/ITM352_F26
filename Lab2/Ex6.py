a4189d720721e63583049af8771278713184fc6a
yelenareyes <yareyes@hawaii.edu>
2026-09-04 12:54:07 -1000

    save

--- Lab2/Ex6.py
+++ Lab2/Ex6.py
@@ -5,13 +5,12 @@
 print(float(input("Enter weight in pounds:"))*0.453592)
 
 KG_TO_POUNDS = 0.453592
-weight_in_pounds = input("Enter weight in pounds: ")
-weight_in_kilograms = float(weight_in_pounds) 
+weight_in_pounds_float = input("Enter weight in pounds: ")
+weight_in_kilograms = float(weight_in_pounds_float) 
 weight_in_kilograms = weight_in_kilograms * KG_TO_POUNDS
 
 print("You entered:", weight_in_pounds_float)
 print("The equivalent weight in kilograms is:", weight_in_kilograms)
-
 
 
                          
